2227971bedf8bc981dd2feebe8ef93ff7045c1b6
rottenpearr <ponchiktyan@gmail.com>
2024-07-22 00:11:43 +0500

    Creating header, components and input fields (no layout)

--- 1.py
+++ 1.py
@@ -1,9 +1,12 @@
 import sys
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout, QGridLayout, QLineEdit, QSpacerItem,
+                               QSizePolicy, QMainWindow)
+from PySide6.QtCore import Qt
+from PySide6 import QtCore, QtGui
 from settings import *
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -13,56 +16,67 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Размеры окна
         self.resize(1000, 800)
-        self.setMinimumSize(800, 600)
-        self.setMaximumSize(1000, 800)
 
         # Стиль окна
         self.setStyleSheet(global_bg)
 
-        # Флаги для окна
-        # Окно всегда поверх других
+        # Флаги для окна (окно всегда поверх других)
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
-        # Заголовок для ввода данных
-        self.data_input = QtWidgets.QLabel("Ввод данных")
-        self.data_input.setFont(title_font)
-        self.data_input.setStyleSheet(color_font)
-        self.data_input.setAlignment(QtCore.Qt.AlignLeft)
-        self.data_input.setContentsMargins(0, 0, 0, 0)
-
-        # Переменные для ввода
-        variables = ["t<sub>0</sub>", "t<sub>k</sub>", "dt<sub>1</sub>", "dt<sub>2</sub>",
-                     "x<sub>0</sub>", "v<sub>0</sub>", "w<sub>0</sub>", "y<sub>0</sub>"]
-        self.labels = []
-        for var in variables:
-            label = QtWidgets.QLabel(var)
-            label.setFont(title_font)
-            label.setStyleSheet(color_font)
-            label.setAlignment(QtCore.Qt.AlignLeft)
-            label.setContentsMargins(0, 0, 0, 0)
-            self.labels.append(label)
-
         # Создание центрального виджета
-        central_widget = QtWidgets.QWidget()
+        central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        # Создание основной компоновки
-        main_layout = QtWidgets.QVBoxLayout(central_widget)
-        main_layout.addWidget(self.data_input)
-        main_layout.setContentsMargins(20, 20, 20, 20)  # Отступы для main_layout
+        # Создание вертикальной левой компоновки (она зависит от центрального виджета)
+        left_vertical_layout = QVBoxLayout(central_widget)
 
-        # Создание компоновки для элементов вводимых данных
-        layout_data = QtWidgets.QVBoxLayout()
-        main_layout.addLayout(layout_data)
+        # Создание компоновки для заголовка ввода данных
+        data_label_layout = QVBoxLayout()
 
-        for label in self.labels:
-            layout_data.addWidget(label)
+        # Создание компоновки для элементов ввода
+        data_input_layout = QGridLayout()
+
+        # Заголовок для ввода данных
+        self.data_input = QLabel("Ввод данных")
+        self.data_input.setFont(title_font)
+        self.data_input.setStyleSheet(color_font)
+        data_label_layout.addWidget(self.data_input, alignment=Qt.AlignTop)
+
+        # Переменные для ввода
+        variables_data = ["t<sub>0</sub>", "t<sub>k</sub>", "dt<sub>1</sub>", "dt<sub>2</sub>",
+                          "x<sub>0</sub>", "v<sub>0</sub>", "w<sub>0</sub>", "y"]
+        self.labels = []
+        self.inputs = []
+
+        for index, element in enumerate(variables_data):
+            # Создание виджета для элемента
+            label = QLabel(element)
+            label.setFont(title_font)
+            label.setStyleSheet(color_font)
+            self.labels.append(label)
+
+            # Создание виджета для поля элемента
+            input_field = QLineEdit()
+            input_field.setFont(title_font)
+            input_field.setStyleSheet(input_bg)
+            self.inputs.append(input_field)
+
+            data_input_layout.addWidget(label, index, 0)
+            data_input_layout.addWidget(input_field, index, 1)
+
+        # Создание спейсера для разграничивания компоновок в вертикальной левой компоновке
+        spacer_1 = QSpacerItem(0, 0, QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        # Добавление компоновок и спейсера в основную
+        left_vertical_layout.addLayout(data_label_layout)
+        left_vertical_layout.addItem(spacer_1)
+        left_vertical_layout.addLayout(data_input_layout)
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QApplication(sys.argv)
 
     window = MainWindow()
     window.show()
 
-    app.exec()
+    sys.exit(app.exec())
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ from PySide6 import QtGui
 
 # Заготовки для шрифтов и цветов
 global_bg = "background-color: #FFF3EB;"
+input_bg = "background-color: #FFF3EB; color: #E0AB9A;"
 title_font = QtGui.QFont("Montserrat", 16, QtGui.QFont.Bold)
 color_font = "color: #4D8FC3;"
 
